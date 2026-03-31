855b735b4271012a2f811d490b356227c8134571
penyelamatbumi <inihanip7@gmail.com>
2026-03-31 21:51:23 +0800

    menambahkan comit

--- looping.cpp
+++ looping.cpp
@@ -205,6 +205,75 @@ cout<<"Bilangan acak do-while yang terakhir ="<<X <<endl;
 cout<<endl;
 }
 
+//Logika not
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60))
+        status = "Lulus";
+    else 
+        status = "tidak lulus";
+    
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
+
+//Logika OR
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else 
+        status = "tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
+
+//Logika AND
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+    
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
+
 
 
 
